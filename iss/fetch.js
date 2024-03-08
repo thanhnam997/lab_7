ddab5e4cacfc6baff7dc57532ddebde6dd6042b2
thanhnam997 <156266844+thanhnam997@users.noreply.github.com>
2024-03-08 08:24:56 -0600

    Update fetch.js and styles.css

--- iss/fetch.js
+++ iss/fetch.js
@@ -1,17 +1,26 @@
 let url = 'https://api.wheretheiss.at/v1/satellites/25544';
 
-let issLat = document.querySelector('#iss-lat')
-let issLong = document.querySelector('#iss-long')
+let issLat = document.querySelector('#iss-lat');
+let issLong = document.querySelector('#iss-long');
+let lastUpdated = document.querySelector('#last-updated');
+let update = 10000; // 10 seconds
+let issMarker; // leaflet marker
+let map = L.map('iss-map').setView([0, 0], 1); // center at 0,0 and max zoom
 
-let update = 10000;
-let issMarker //leaflet marker
-let map = L.map('iss-map').setView([0, 0], 1) //center at 0,0 and max zoom
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-iss() //intial call funtion
-setInterval(iss,update)//call iss funtion update every seconds
+// Custom ISS icon
+let customIcon = L.icon({
+    iconUrl: 'noun-iss-956251.svg', // Replace with the actual path to your ISS icon
+    iconSize: [32, 32],
+    iconAnchor: [16, 16],
+    popupAnchor: [0, -16]
+});
+
+iss(); // initial call function
+setInterval(iss, update); // call iss function update every second
 
 function iss() {
     fetch(url)
@@ -20,24 +29,22 @@ function iss() {
             console.log(issData);
             let lat = issData.latitude;
             let long = issData.longitude;
+            let timestamp = new Date(issData.timestamp * 1000); // Convert UNIX timestamp to milliseconds
             issLat.innerHTML = lat;
             issLong.innerHTML = long;
 
             // create marker if does not exist
             // move marker if it does exist
             if (!issMarker) {
-                // create marker
-                issMarker = L.marker([lat, long]).addTo(map);
+                issMarker = L.marker([lat, long], { icon: customIcon }).addTo(map); // Create the marker
             } else {
-                // move marker
-                issMarker.setLatLng([lat, long]);
+                issMarker.setLatLng([lat, long]); // Already exists — move to a new location
             }
+
+            // Update timestamp
+            lastUpdated.innerHTML = 'Last Updated: ' + timestamp.toLocaleString();
         })
-        .catch((err) => {
-            console.log('ERROR!', err);
+        .catch(function (err) {
+            console.error('ERROR!', err.message);
         });
 }
-
-// Initial call and set up interval
-iss();
-setInterval(iss, update);
